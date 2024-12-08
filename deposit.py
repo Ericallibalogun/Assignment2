99ae2e9eafb89857666e8087f5e30cce454961a1
Ericalli <allieric28@gmail.com>
2024-12-08 20:05:42 +0100

    new addition

---
+++ deposit.py
@@ -0,0 +1,18 @@
+deposit: 300
+deposit2: 300
+withdraw: 200
+deposit: 100
+Balance: 500
+match deposit:
+	case 1:deposit
+Deposit:300
+Deposit:300
+Deposit:100
+
+match withdraw:
+	case 1 : withdraw 
+Withdraw: 200
+
+match balance:
+	case 3: Balance
+Your Balance is 500
---
+++ task1.py
@@ -0,0 +1,6 @@
+radius = 2
+pi = 3.14159
+
+print : (' Area' , pi * radius * radius)
+print : ('Diameter', 2 * radius)
+print : ('Circumference', 2 * pi * radius)
